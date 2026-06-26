baa99a9c0df5585287a4b70c0c8939c5498877b8
Lucas E. R. <56853712+lucasestevesr@users.noreply.github.com>
2026-06-26 13:31:40 -0300

    codecrafters submit [skip ci]

--- src/Program.cs
+++ src/Program.cs
@@ -15,6 +15,12 @@ class Program
                 Console.WriteLine(command.Substring(5));
                 continue;
             }
+            else if (command.StartsWith("type "))
+            {
+                if(command.Substring(5).Equals("echo") || command.Substring(5).Equals("exit"))
+                    Console.WriteLine($"{command.Substring(5)} is a shell builtin");
+                continue;
+            }
             Console.WriteLine($"{command}: command not found");
             //Console.Clear();
         }
